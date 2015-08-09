ee414b09a1f3492de09681ef62d2f4256fb2bffd
Zack Harris <zack.tibia@gmail.com>
2015-08-09 06:22:47 -0400

    oh fuck maybe I should commit this

---
+++ Assets/PathGravity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathGravity : MonoBehaviour {
+    public bool ApplyGravity = true;
+    public float gravityMultiplier = 10f;
+    void Start () {
+        var cc = GetComponent<CharacterController>();
+        
+    }
+	void Update () {
+	
+	}
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    Physics.gravity = -collision.contacts[0].normal * gravityMultiplier;
+    //}
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (ApplyGravity)
+        {
+            Physics.gravity = -hit.normal * gravityMultiplier;
+        }
+    }
+    void OnDrawGizmos()
+    {
+        DrawArrow.ForGizmo(transform.position, Physics.gravity, Color.red);
+    }
+}
---
+++ Assets/TunnelSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TunnelSpawner : MonoBehaviour {
+    List<GameObject> tunnels;
+	// Use this for initialization
+	void Start () {
+        SpawnTunnels();
+    }
+    void SpawnTunnels()
+    {
+        tunnels = new List<GameObject>();
+        int amount = Random.Range(1, 2);
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject tun = new GameObject("Tunnel" + i);
+            tun.transform.parent = transform.parent;
+            tun.AddComponent<TunnelGenerator>();
+            tunnels.Add(tun);
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(1))
+        {
+            foreach(GameObject t in tunnels)
+            {
+                Destroy(t);
+            }
+            SpawnTunnels();
+
+            var player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                player.transform.position = new Vector3(0, 5, 0);
+                player.transform.rotation = Quaternion.identity;
+                Physics.gravity = Vector3.down;
+            }
+        }
+	}
+}
